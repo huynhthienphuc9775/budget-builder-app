657a61e0f09becff00213bd53a66ee49daf42194
YaphetS <phucht@brickmate.vn>
2024-12-10 17:27:12 +0700

    push again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DateRangePickerComponent } from './components/date-range-picker/date-range-picker.component';
+import { BudgetTableComponent } from './components/budget-table/budget-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    DateRangePickerComponent,
+    BudgetTableComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+
 })
 export class AppComponent {
-  title = 'budget-builder';
+  public months = { start: '01/2024', end: '12/2024' };
+
+
+  receiveMonths(event: any) {
+    this.months = event;
+  }
+
 }
